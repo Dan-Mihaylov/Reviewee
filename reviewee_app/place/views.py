bd5047bef167a277d625cfd1f6af27fba8a1b38e
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-03-14 02:11:02 +0000

    Restaurant Add View
    Created the view, template and functionality for adding a new restaurant.

--- reviewee_app/place/views.py
+++ reviewee_app/place/views.py
@@ -1,8 +1,9 @@
 from django.forms import modelform_factory
 from django.shortcuts import render, HttpResponse
+from django.urls import reverse
 from django.views import generic as views
 
-from .models import BasePlaceModel
+from .models import BasePlaceModel, Restaurant
 from ..account.mixins import BusinessOwnerRequiredMixin
 
 
@@ -12,7 +13,18 @@ class PlaceAddView(BusinessOwnerRequiredMixin, views.TemplateView):
 
 
 class RestaurantAddView(views.CreateView):
-    pass
+    template_name = 'place/restaurant-add.html'
+    form_class = modelform_factory(Restaurant, fields='__all__')
+
+    # TODO: add the user automatically, don't select it.
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+    def get_success_url(self):
+        # you get the object in the form_valid() in the CreateView
+        return reverse('home')
 
 
 class HotelAddView(views.CreateView):
