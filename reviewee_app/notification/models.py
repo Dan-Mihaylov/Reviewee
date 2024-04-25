745620d690c15257c8a9c4f18cc30e9847efd77c
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-04-25 19:58:58 +0100

    Created Notification Model
    Created the notifications model

--- reviewee_app/notification/models.py
+++ reviewee_app/notification/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from reviewee_app.common.models import AuditModelMixin
+
+
+UserModel = get_user_model()
+
+
+class Notification(AuditModelMixin, models.Model):
+    MAX_LENGTH_TEXT_FIELD = 500
+
+    user = models.ForeignKey(
+        UserModel,
+        related_name='notifications',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
+
+    text = models.TextField(
+        max_length=MAX_LENGTH_TEXT_FIELD,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f'Notification for {self.user}'
